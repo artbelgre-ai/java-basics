fe737ef83a516a919aeb5eda5d9c8b54cfd94312
Arthur Belamino Gregorio <artbelgre@gmail.com>
2026-03-28 15:17:10 -0400

    Add IfExemple.java

---
+++ IfExemple.java
@@ -0,0 +1,11 @@
+public class IfExample {
+    public static void main(String[] args) {
+        int age = 32;
+
+        if (age >= 32) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are underage.");
+        }
+    }
+}
