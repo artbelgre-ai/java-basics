4f9548c19bc77673d22a3d057926382126f75a7b
Arthur Belamino Gregorio <artbelgre@gmail.com>
2026-03-28 15:11:36 -0400

    Add VariablesExemple.java

---
+++ VariablesExemple.java
@@ -0,0 +1,10 @@
+publlic classs VariablesExemple {
+  public static void main(String[]args) {
+  String name = " Arthur";
+  int age = 32;
+  doble heiigth = 1.85;
+  System.out.println("Neme: ' + name);
+  System.out.println("Age: ' + age);
+  System.out.println("Heigth: ' + heigth);
+   }
+}
